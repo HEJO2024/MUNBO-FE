1cad507be8dba258269d3af0826e29b26654da5a
YeomSeunghyeon <100lion67@gmail.com>
2024-04-28 20:13:54 +0900

    0428

--- src/page/Quiz/QuizEdit.js
+++ src/page/Quiz/QuizEdit.js
@@ -13,7 +13,10 @@ const QuizEdit=()=>{
         keywordId:"",
         quizContent:"",
         quizImg:"",
-        answ:"",
+        answ_1:"",
+        answ_2:"",
+        answ_3:"",
+        answ_4:"",
          r_answ:"",
          wrgAnsw_explanation:"",
       })
@@ -33,7 +36,10 @@ const QuizEdit=()=>{
                 keywordId:userData.keywordId,
                 quizContent:userData.quizContent,
                 quizImg:userData.quizImg,
-                answ:JSON.parse(userData.answ),
+                answ_1:userData.answ_1,
+                answ_2:userData.answ_2,
+                answ_3:userData.answ_3,
+                answ_4:userData.answ_4,
                 r_answ:userData.r_answ,
                 wrgAnsw_explanation:userData.wrgAnsw_explanation,
              });
@@ -57,7 +63,10 @@ const QuizEdit=()=>{
         keywordId:data.keywordId,
         quizContent:data.quizContent,
         quizImg:data.quizImg,
-        answ:JSON.stringify(data.answ),
+        answ_1:data.answ_1,
+        answ_2:data.answ_2,
+        answ_3:data.answ_3,
+        answ_4:data.answ_4,
         r_answ:data.r_answ,
         wrgAnsw_explanation:data.wrgAnsw_explanation
        
@@ -89,16 +98,7 @@ const QuizEdit=()=>{
     }));
 
 }; //정보 수정 입력하면 바뀌는 함수
-const handleAnswChange = (event, key) => {
-    const { value } = event.target;
-    setData(prevState => ({
-        ...prevState,
-        answ: {
-            ...prevState.answ,
-            [key]: value
-        }
-    }));
-};
+
 return(
     <div className="AdminEntire">
     <AdminHeader/>
@@ -111,17 +111,26 @@ return(
 
     
     <div className="AnswAll">
-            {data && Object.keys(data.answ).map(key => (
-                <div key={key} className="Answ">
-                    <div className="AnswKey">{key}</div>
-                    <textarea className="AnswInput"
-                        type="text" 
-                        value={data.answ[key]} 
-                        style={{ resize: 'none' }}
-                        onChange={(e) => handleAnswChange(e, key)} 
-                    />
+                <div className="Answ">
+                    <div className="AnswKey">1:</div>
+                    <textarea className="AnswInput"  name="answ_1" type="text" value={data.answ_1} 
+                     style={{ resize: 'none' }} onChange={handleChange} />
                 </div>
-            ))}
+                <div className="Answ">
+                    <div className="AnswKey">2:</div>
+                    <textarea className="AnswInput"  name="answ_2" type="text" value={data.answ_2} 
+                     style={{ resize: 'none' }} onChange={handleChange} />
+                </div>
+                <div className="Answ">
+                    <div className="AnswKey">3:</div>
+                    <textarea className="AnswInput"  name="answ_3" type="text" value={data.answ_3} 
+                     style={{ resize: 'none' }} onChange={handleChange} />
+                </div>
+                <div className="Answ">
+                    <div className="AnswKey">4:</div>
+                    <textarea className="AnswInput"  name="answ_4" type="text" value={data.answ_4} 
+                     style={{ resize: 'none' }} onChange={handleChange} />
+                </div>
         </div>
         <div className="R_answ"><div>정답</div>
         <input name="r_answ" value={data.r_answ} onChange={handleChange} className="R_answInput"></input> </div>
