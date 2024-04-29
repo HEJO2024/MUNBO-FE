caa132a9c0418585eece8c65a9abe78941f193e4
YeomSeunghyeon <100lion67@gmail.com>
2024-04-29 11:39:38 +0900

    0429

--- src/page/Home.js
+++ src/page/Home.js
@@ -22,15 +22,15 @@ const Home=()=>{
         <div className="MainSub">
         <a href="/admin/login" >로그인 하러 가기</a></div></div>:
         <div className="MainMenu">
-             <Link to="/admin/user/view" className="item"><img src={process.env.PUBLIC_URL + '/user.png'} width = '50px' className="itemLogo" />
+             <Link to="/admin/user/view" className="item"><img src={process.env.PUBLIC_URL + '/user.png'} width = '80px' className="itemLogo" />
             <div>회원관리</div></Link>
-            <Link to="/admin/quiz/view" className="item1"><img src={process.env.PUBLIC_URL + '/quiz.jpg'} width = '50px' className="itemLogo1" />
+            <Link to="/admin/quiz/view" className="item1"><img src={process.env.PUBLIC_URL + '/quiz.jpg'}    width = '80px' className="itemLogo1" />
             <div>기출문제관리</div></Link>
-            <Link to="/admin/content/select" className="item"><img src={process.env.PUBLIC_URL + '/search.png'} width = '50px' className="itemLogo" />
+            <Link to="/admin/content/select" className="item"><img src={process.env.PUBLIC_URL + '/search.png'} width = '80px' className="itemLogo" />
             <div>키워드/과목/회차 관리</div></Link>
-            <div className="item"><img src={process.env.PUBLIC_URL + '/pi.png'} width = '50px' className="itemLogo" />
+            <div className="item"><img src={process.env.PUBLIC_URL + '/pi.png'} width = '80px' className="itemLogo" />
             <div>통계/평가</div></div>
-            <div className="item"><img src={process.env.PUBLIC_URL + '/GptLogo.png'} width = '50px' className="itemLogo" />
+            <div className="item2"><img src={process.env.PUBLIC_URL + '/GptLogo.png'} width = '80px' className="itemLogo" />
             <div>Gpt4 토큰 사용량 조회</div></div>
         </div>
 }
